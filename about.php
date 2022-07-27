9e25db8fcf2e011c41c2974d4155ab3748a89b76
Falcon Player <fpp@FPP>
2022-07-27 13:31:56 -0500

    cleanup tags

--- about.php
+++ about.php
@@ -2,15 +2,16 @@
     <fieldset style="padding: 10px; border: 2px solid #000;">
       <legend>NFL Template Info</legend>
       <div style="overflow: hidden; padding: 10px;">
-    <div>
-      <div id='credits'>
-        <b>Template Plugin Developed By:</b><br />
-		<br />
-        Ben Kools (koolsb)<br />
-		<br />
-        <a href='https://github.com/koolsb/fpp-nfl'>Git Repository</a><br>
-		<br />
+        <div>
+          <div id='credits'>
+            <b>Template Plugin Developed By:</b><br />
+		        <br />
+            Ben Kools (koolsb)<br />
+		        <br />
+            <a href='https://github.com/koolsb/fpp-nfl'>Git Repository</a><br>
+		        <br />
+          </div>
+        </div>
       </div>
-    </div>
     </fieldset>
   </div>
--- help/content.php
+++
@@ -1,1 +0,0 @@
-<h2>The context-sensitive Help page for the plugin's content.php page</h2>
