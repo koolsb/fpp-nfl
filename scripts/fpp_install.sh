d06050b5886eb0446dd21704ac4470210751fbd6
Poporacer <46263877+Poporacer@users.noreply.github.com>
2022-09-10 20:12:22 -0600

    Update fpp_install.sh

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -3,8 +3,11 @@
 # fpp-nfl install script
 
 # Mark to reboot
+
+pushd $(dirname $(which $0))
+target_PWD=$(readlink -f .)
 /opt/fpp/scripts/update_plugin ${target_PWD##*/}
-echo ; echo “Please reboot fppd” ; echo
+echo ; echo “Please reboot fppd.” ; echo
 . /opt/fpp/scripts/common
 setSetting rebootFlag 1
 popd
