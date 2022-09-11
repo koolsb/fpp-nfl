aafdff2374002f084e1ae293cb923531aa677da4
Poporacer <46263877+Poporacer@users.noreply.github.com>
2022-09-10 19:29:11 -0600

    Set Reboot flag

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -4,3 +4,7 @@
 
 # Mark to reboot
 sed -i -e "s/^restartFlag .*/restartFlag = 1/" /home/fpp/media/settings
+/opt/fpp/scripts/update_plugin ${target_PWD##*/}
+echo ; echo “Please reboot fppd” ; echo
+. /opt/fpp/scripts/common
+setSetting rebootFlag 1
