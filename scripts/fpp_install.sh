2bff8f904d9448b4f30c8c309897067fab028220
Poporacer <46263877+Poporacer@users.noreply.github.com>
2022-09-10 19:29:37 -0600

    Fix Reboot settings

--- scripts/fpp_install.sh
+++ scripts/fpp_install.sh
@@ -3,7 +3,6 @@
 # fpp-nfl install script
 
 # Mark to reboot
-sed -i -e "s/^restartFlag .*/restartFlag = 1/" /home/fpp/media/settings
 /opt/fpp/scripts/update_plugin ${target_PWD##*/}
 echo ; echo “Please reboot fppd” ; echo
 . /opt/fpp/scripts/common
